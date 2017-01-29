ea398722225f318f54d1b1f4b18466aa539e3803
冯涛 <1508907793@qq.com>
2017-01-29 16:08:09 +0800

    实现切换文字功能，添加样式

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,5 +1,21 @@
+window.onload = countBodyChildren;
+
+// 展示图片
+// @whichPic 超链接节点
 function showPic(whichPic) {
+	// 切换图片
 	var source = whichPic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
+
+	// 切换文字
+	var text = whichPic.getAttribute("title");
+	var description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
+}
+
+// 计算body中的子节点
+function countBodyChildren() {
+	var body_element = document.getElementsByTagName("body")[0];
+	console.log(body_element.childNodes.length);
 }
