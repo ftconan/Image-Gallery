b341a2798c30758e44a248a0b1855976cb743b1a
冯涛 <1508907793@qq.com>
2017-02-03 11:08:38 +0800

    将html与js分离，去除js对html的依赖

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,20 +1,57 @@
-window.onload = countBodyChildren;
+// 当网页加载完毕之后出发onload事件
+window.onload = function() {
+	prepareGallery();
+	countBodyChildren();
+};
 
-// 展示图片
+// 展示图片函数
 // @whichPic 超链接节点
+// return true;
 function showPic(whichPic) {
+	// 检查是否有placeholder
+	if (!document.getElementById("placeholder")) {
+		return false;
+	}
+
 	// 切换图片
 	var source = whichPic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
 
 	// 切换文字
-	var text = whichPic.getAttribute("title");
-	var description = document.getElementById("description");
-	description.firstChild.nodeValue = text;
+	if (document.getElementById("description")) {
+		var text = whichPic.getAttribute("title");
+		var description = document.getElementById("description");
+		description.firstChild.nodeValue = text;
+	}
+
+	return true;
 }
 
-// 计算body中的子节点
+// 图片库预处理函数
+function prepareGallery() {
+	// 如果没有以下方法，不执行后续程序
+	if (!document.getElementsByTagName) {
+		return false;
+	}
+	if (!document.getElementById) {
+		return false;
+	}
+	if (!document.getElementById("imagegallery")) {
+		return false;
+	}
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = function() {
+			// this 代表 links[i]
+			// 如果showPic返回true,我们就返回false，浏览器不会打开那个链接
+			return !showPic(this);      
+		};
+	}
+}
+
+// 计算body中的子节点函数
 function countBodyChildren() {
 	var body_element = document.getElementsByTagName("body")[0];
 	console.log(body_element.childNodes.length);
